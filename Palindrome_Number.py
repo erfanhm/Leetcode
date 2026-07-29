fb31b3ff00ed6e82e7960a2bb2380ad02be926c9
ErfanHamidi <155196493+erfanhm@users.noreply.github.com>
2026-07-29 12:09:34 -0400

    Simple way which is changing the int into str

---
+++ Palindrome_Number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x:int)-> bool:
+        return str(x) == str(x)[::-1]
+solution = Solution()
+print (solution.isPalindrome(121))
+print (solution.isPalindrome(-121))
+print (solution.isPalindrome(232))
+print (solution.isPalindrome(789))
