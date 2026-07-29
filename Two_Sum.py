48811d440099bdfe1bc2f4b6a7960682b178337e
ErfanHamidi <155196493+erfanhm@users.noreply.github.com>
2026-07-29 11:55:24 -0400

    This version includes the Time complexity O(n)

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,3 +1,4 @@
+#--------Time complexity : O(n^2)---------
 from typing import List
 class Solution:
     def two_sum (self, nums: List[int],target: int) -> List[int]:
@@ -9,3 +10,21 @@ class Solution:
 
 tSum = Solution()
 print(tSum.two_sum([2,4,7,9], 9)) 
+
+#--------Time complexity : O(n)-----------
+class SolutionSec:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i, number in enumerate(nums):
+            needed = target - number
+
+            if needed in seen:
+                return [seen[needed], i]
+
+            seen[number] = i
+        
+        return []
+
+tSumSec = SolutionSec()
+print(tSumSec.two_sum([3,6,2,1,8,4], 10))
