e95efd97bd6384e7c6984a86c0b8083f15e11db7
ErfanHamidi <155196493+erfanhm@users.noreply.github.com>
2026-07-29 12:57:09 -0400

    Added second solution

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -1,7 +1,24 @@
+#-----------------Time complexity O(n)---------------
 class Solution:
     def isPalindrome(self, x:int)-> bool:
         return str(x) == str(x)[::-1]
 solution = Solution()
+print (solution.isPalindrome(121))
+print (solution.isPalindrome(-121))
+print (solution.isPalindrome(232))
+print (solution.isPalindrome(789))
+
+#-------------Time complexity O(n) but with reversing the number not changing it to string----------
+class SolutionSec:
+    def isPalindrome(self, x: int) -> bool:
+        original = x
+        reverse = 0
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x = x // 10
+        return original == reverse
+solution = SolutionSec()
 print (solution.isPalindrome(121))
 print (solution.isPalindrome(-121))
 print (solution.isPalindrome(232))
